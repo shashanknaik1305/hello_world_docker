74deb5cf818db7e564db8b252c7afe01f6c89503
Shashank Naik <shashanknaik380@example.com>
2025-10-17 11:44:23 +0000

    trigger build

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World! 🚀 Updated Version v2 from Automated Build"
+    return "Hello, weird World! 🚀 Updated Version v2 from Automated Build"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
