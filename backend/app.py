b7ea487ede751fe832b20d2fee044b826adf3467
Shashank Naik <shashanknaik380@example.com>
2025-10-17 13:08:16 +0000

    triggered new1 commit with cd

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, shashank! 🚀 Updated Version v2 from Automated Build"
+    return "Hello, shashank from ! 🚀 Updated Version v2 from Automated Build"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
